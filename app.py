ace773b1fb90b96d54f0b39b73bcee6ea67c2394
Anuj Asati <61572455+Anuj-Asati19@users.noreply.github.com>
2023-10-31 18:29:31 +0530

    3rd Commit

--- app.py
+++ app.py
@@ -1,37 +1,19 @@
-from flask import Flask, request, jsonify, render_template,session,redirect
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-import secrets
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:shivang123@localhost/sportify'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://AnujAsati:Anujasati19@localhost/Sportify'
+
 db = SQLAlchemy(app)
-app.secret_key = secrets.token_hex(16)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    sports_preference = db.Column(db.String(255),nullable=False)
     
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/login',methods=['GET','POST'])
-def login():
-    if request.method == 'POST':
-        data = request.form
-        existing_user = User.query.filter_by(email=data['email']).first()
-        if existing_user:
-            if existing_user.username != data['username']:
-                return render_template('login.html', alert_message='Username does not match the provided email.')
-            else:
-                session['user_id'] = existing_user.id
-                return render_template('home.html',user_id=existing_user.id)
-        else:
-            return render_template('login.html', alert_message='User does not exist')
-    else:
-        return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -39,46 +21,22 @@ def register():
         data = request.form
         existing_user = User.query.filter_by(email=data['email']).first()
         if existing_user:
-            return render_template('register.html', alert_message='User already exist')
+            # User with the provided email exists, check if the username matches
+            if existing_user.username != data['username']:
+                # Username does not match, show an alert
+                return render_template('register.html', alert_message='Username does not match the provided email.')
+            else:
+                # Username matches, log the user in
+                return render_template('home.html')
         else:
-            new_user = User(email=data['email'], username=data['username'],sports_preference=data['sport'])
+            # User does not exist, create a new user
+            new_user = User(email=data['email'], username=data['username'])
             db.session.add(new_user)
             db.session.commit()
-            session['user_id'] = new_user.id
-            return render_template('home.html',user_id=new_user.id)
+            return render_template('home.html')
     else:
         # Handle GET request for the registration form page here (if needed)
         return render_template('register.html')
-
-@app.route('/profile/<int:user_id>')
-def profile(user_id):
-    user = User.query.get(user_id)
-    return render_template('profile.html', user=user)
-
-@app.route('/update_preferences', methods=['POST'])
-def update_preferences():
-    user_id = session.get('user_id')
-    if user_id:
-        user = User.query.get(user_id)
-        if user:
-            # Get sports preference from the form data
-            sports_preference = request.form.get('sports_preference')
-            # Update the sports_preference column in the database
-            user.sports_preference = sports_preference
-
-            # Commit the changes to the database
-            db.session.commit()
-
-            # Redirect to the profile page after updating preferences
-            return redirect('/profile')
-        else:
-            # Handle case where user does not exist
-            pass
-    else:
-        # Handle case where user is not logged in
-        pass
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- reg.py
+++ reg.py
@@ -1,32 +1,52 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
-app = Flask(__name__)
+app = Flask(__name)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/sportify'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        data = request.get_json()
-        new_user = User(email=data['email'], username=data['username'])
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify(message='User registered successfully')
-    else:
-        # Handle GET request for the registration form page here (if needed)
-        return jsonify(message='Invalid request method')
-
-
-# Additional routes for profile management can be added here
 class Sport(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user1_id = db.Column(db.Integer, nullable=False)
+    user2_id = db.Column(db.Integer, nullable=False)
+    sport_id = db.Column(db.Integer, nullable=False)
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data.get('email')
+    username = data.get('username')
+    password = data.get('password')
+
+    if not email or not username or not password:
+        return jsonify(message='Invalid data'), 400
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify(message='User already exists'), 409
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    new_user = User(email=email, username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(message='User registered successfully')
+
+@app.route('/register', methods=['GET'])
+def render_register_page():
+    return render_template('register.html')
 
 @app.route('/sports', methods=['GET'])
 def get_sports():
@@ -34,20 +54,21 @@ def get_sports():
     sport_list = [{'id': sport.id, 'name': sport.name} for sport in sports]
     return jsonify(sports=sport_list)
 
-
-class Match(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user1_id = db.Column(db.Integer, nullable=False)
-    user2_id = db.Column(db.Integer, nullable=False)
-    sport_id = db.Column(db.Integer, nullable=False)
-    # Add more fields like date, time, location, etc.
-
 @app.route('/create_match', methods=['POST'])
 def create_match():
     data = request.get_json()
-    new_match = Match(user1_id=data['user1_id'], user2_id=data['user2_id'], sport_id=data['sport_id'])
+    user1_id = data.get('user1_id')
+    user2_id = data.get('user2_id')
+    sport_id = data.get('sport_id')
+
+    if not user1_id or not user2_id or not sport_id:
+        return jsonify(message='Invalid data'), 400
+
+    new_match = Match(user1_id=user1_id, user2_id=user2_id, sport_id=sport_id)
     db.session.add(new_match)
     db.session.commit()
     return jsonify(message='Match created successfully')
 
-# Additional routes for listing and managing matches can be added here
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
