97eb3910fb70217502b879bf0d2a7166a1ef1a77
theextraordinary <shivangsharma074@gmail.com>
2023-11-14 22:36:25 +0530

    checking

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    # sports_preference = db.Column(db.String(255),nullable=False)
+    # sports_preference = db.Column(db.String(255),nullable=False)   
     
 @app.route('/')
 def index():
