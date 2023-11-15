96f484cbe95de8d704b8f0639b2c6a5f24c83ac1
Anuj Asati <61572455+Anuj-Asati19@users.noreply.github.com>
2023-11-15 07:30:18 +0530

    added Docker support

--- reg.py
+++ reg.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:johncena@localhost/Sportify'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
 
 class User(db.Model):
